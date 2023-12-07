56b7c8c96eb5e378b069c1eab26b7c585856f8b6
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-12-07 23:15:12 +0800

    chore: refactor Dockerfiles and update dependencies
    - Modify `Dockerfile.01`, `Dockerfile.02`, and `Dockerfile.03` to include `go.mod` and `go.sum` files
    - Update `go.mod` to include new dependencies
    - Remove unnecessary code in `main.go` and replace it with a new route handler for `/ping` endpoint
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
