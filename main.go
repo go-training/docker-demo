c572df03d033b66d92780577e21e71dd0bce3345
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-12-07 22:39:59 +0800

    refactor: refactor HTTP routing and remove Gin dependency
    - Remove the dependency on `github.com/gin-gonic/gin` in `go.mod`
    - Replace the Gin router and handler with a simple HTTP handler function in `main.go`
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
